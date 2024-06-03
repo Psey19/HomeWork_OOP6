bc825069cc9dbde917034ef5d29f9dd8af05f3ec
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-06-04 00:59:31 +0500

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,9 +1,7 @@
 package Task_1;
 
 import Task_1.Controller.StudentController;
-import Task_1.Model.Student;
-import Task_1.Model.UniversityPerson;
-import Task_1.Model.User;
+
 
 
 public class Program {
@@ -17,13 +15,9 @@ public class Program {
         System.out.println("-----------------------------");
         controller.printSortedStudentList();
         System.out.println("-----------------------------");
+        controller.printStudent(controller.getStudent(3));
 
-        UniversityPerson universityPerson = new Student(5, "rt", "frf", "rfrfr");
-        universityPerson.showPerson();
-        System.out.println("-----------------------------");
-        User user = new User(new Student(6,"Grisha", "N.", "J."));
-        user.showPerson();
-        System.out.println("-----------------------------");
+
 
 
 
