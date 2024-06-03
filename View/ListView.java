ec3458c9a9b33a4d8eea5c11866b972e13f5bf6d
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-06-04 00:54:24 +0500

    Update ListView.java

--- View/ListView.java
+++ View/ListView.java
@@ -1,10 +1,10 @@
 package Task_1.View;
 
-import Task_1.Model.UniversityPerson;
+import Task_1.Model.User;
 
 import java.util.List;
 
-interface ListView<T extends UniversityPerson> {
+interface ListView<T extends User> {
     void sendListOnConsole(List<T> users);
 
 }
