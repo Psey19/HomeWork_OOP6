47b67a72100ddd971b00cb211e931f39405c9f55
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-06-04 00:49:09 +0500

    Update and rename UniversityPersonController.java to UserController.java

--- Controller/UserController.java
+++ Controller/UserController.java
@@ -1,7 +1,7 @@
 package Task_1.Controller;
 
-import Task_1.Model.UniversityPerson;
+import Task_1.Model.User;
 
-interface UniversityPersonController <T extends UniversityPerson>{
+interface UserController <T extends User>{
     void create(String firstName, String SecondName, String LastName);
 }
