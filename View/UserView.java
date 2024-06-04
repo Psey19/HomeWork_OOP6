5a405e2db0ab39c47b91d3916836d1c8543b6a3f
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-06-04 12:18:35 +0500

    Update UserView.java

--- View/UserView.java
+++ View/UserView.java
@@ -3,8 +3,7 @@ package Task_1.View;
 import Task_1.Model.User;
 
 import java.util.List;
-//Здесь наглядно продемонстрирован принцип OSP,
-//Данный интерфейс реализует методы интерфейса ListView и расширяет его возможности новым методом
+
 interface UserView<T extends User> extends ListView<T> {
     @Override
     default void sendListOnConsole(List<T> users) {
